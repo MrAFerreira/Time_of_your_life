9faef1a60141ebcc0a15b2ba350f7c4cee7310ff
André Ferreira <mrawesomeferreira@gmail.com>
2020-02-22 14:40:24 +0000

    adding profile routes

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const basicAuthenticationDeserializer = require('./middleware/basic-authenticati
 const bindUserToViewLocals = require('./middleware/bind-user-to-view-locals.js');
 const indexRouter = require('./routes/index');
 const authenticationRouter = require('./routes/authentication');
+const profileRouter = require('./routes/profile');
 
 const app = express();
 
@@ -25,8 +26,7 @@ app.use(
   sassMiddleware({
     src: join(__dirname, 'public'),
     dest: join(__dirname, 'public'),
-    outputStyle:
-      process.env.NODE_ENV === 'development' ? 'nested' : 'compressed',
+    outputStyle: process.env.NODE_ENV === 'development' ? 'nested' : 'compressed',
     force: process.env.NODE_ENV === 'development',
     sourceMap: true
   })
@@ -57,6 +57,7 @@ app.use(bindUserToViewLocals);
 
 app.use('/', indexRouter);
 app.use('/authentication', authenticationRouter);
+app.use('/profile', profileRouter);
 
 // Catch missing routes and forward to error handler
 app.use((req, res, next) => {
---
+++ routes/profile.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const { Router } = require('express');
+const router = new Router();
+
+const User = require('./../models/user');
+const Path = require('./../models/path');
+
+const routeGuard = require('./../middleware/route-guard');
+
+router.get('/edit', (req, res, next) => {
+  res.render('profile/edit');
+});
+
+router.post('/edit', routeGuard(true), (req, res, next) => {
+  const userId = req.user._id;
+  const { name } = req.body;
+
+  User.findByIdAndUpdate(userId, {
+    name
+  })
+    .then(() => {
+      res.redirect('/');
+    })
+    .catch(error => {
+      next(error);
+    });
+});
+
+router.get('/:userId', (req, res, next) => {
+  const { userId } = req.params;
+  let user;
+
+  User.findById(userId)
+    .then(document => {
+      user = document;
+      if (document) {
+        return Path.find({ user: userId });
+      } else {
+        next(new Error('USER_NOT_FOUND'));
+      }
+    })
+    .then(paths => {
+      const isOwnProfile = req.user && req.user._id.toString() === user._id.toString();
+      res.render('profile/single', { profile: user, paths, isOwnProfile });
+    })
+    .catch(error => {
+      next(error);
+    });
+});
+
+module.exports = router;
