871af0ca82cde1b067c78b221e10a25b0ad9a456
RaqueCoelho <57578500+RaqueCoelho@users.noreply.github.com>
2020-02-25 09:52:36 +0000

    done

--- routes/path.js
+++ routes/path.js
@@ -53,9 +53,12 @@ router.get('/sport', (req, res, next) => {
 
 router.get('/wildlife', (req, res, next) => {
   res.render('holdthemes/wildlife');
+});
+
 router.post('/create', routeGuard(true), bindUser, (req, res, next) => {
   const userId = req.user._id;
   const { name, location } = req.body;
+
   Path.create({
     user: userId,
     name,
