3049873799c51fe15beffa7d29fee4a3acdbba21
André Ferreira <mrawesomeferreira@gmail.com>
2020-02-27 14:35:37 +0000

    trying google passport

--- routes/authentication.js
+++ routes/authentication.js
@@ -104,7 +104,7 @@ router.post('/sign-in', (req, res, next) => {
 
 router.get('/google', passport.authenticate('google', { scope: ['profile'] }));
 router.get(
-  '/google-callback',
+  '/google/callback',
   passport.authenticate('google', { failureRedirect: '/' }),
   (req, res) => {
     res.redirect('/');
